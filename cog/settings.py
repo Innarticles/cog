a1ed30cd41e26cd2188743ee9d07f66ef8c1dc4f
Innocent Udeogu <innocentudeh@gmail.com>
2018-10-15 08:41:19 +0100

    done

--- cog/settings.py
+++ cog/settings.py
@@ -74,15 +74,21 @@ WSGI_APPLICATION = 'cog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.contrib.gis.db.backends.postgis',
+#         'NAME': 'cognitiks_dev',
+#         'USER': 'admin',
+#         'PASSWORD': 'root',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'cognitiks_dev',
-        'USER': 'admin',
-        'PASSWORD': 'root',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
